ee24137b6d24d873ccd9abe1f9b4ca8daeaee83d
paul <pavel.parfentii@crisp-studio.com>
2024-02-23 15:52:38 +0200

    discord patrol/og_patrol update1

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\ConstantValues;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class Account extends Model
 {
@@ -38,6 +39,8 @@ class Account extends Model
 
             $originalRole = $account->getOriginal('role');
             $currentRole = $account->role;
+            Log::info($currentRole . $account->id);
+
             $id = $account->id;
 
            $patrol = DB::table('safe_souls')
@@ -76,6 +79,7 @@ class Account extends Model
                    'query_param' => ConstantValues::safesoul_og_patrol_role
                ]);
            }elseif (!isset($og_patrol) && !isset($patrol) && !is_null($currentRole)){
+               Log::info('here' . $id);
                if($currentRole === ConstantValues::safesoul_og_patrol_role){
                    SafeSoul::create([
                        'account_id'=>$id,
