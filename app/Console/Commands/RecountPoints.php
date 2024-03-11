3fc6ee4bcf808f2341a5463feaf54b84b3db28f0
paul <pavel.parfentii@crisp-studio.com>
2024-03-11 21:09:06 +0200

    added recount command

---
+++ app/Console/Commands/RecountPoints.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Account;
+use Illuminate\Console\Command;
+
+class RecountPoints extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:recount-points';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $accounts = Account::with('animals', 'twitters')->get();
+
+        foreach ($accounts as $account) {
+            // Calculate the sum of points from digital animals
+            $totalDigitalAnimalPoints = $account->digitalAnimals->sum('points');
+
+            // Update the account's total_points
+            $account->total_points = $totalDigitalAnimalPoints;
+            $account->save();
+        }
+    }
+}
