a258357b12eacea529c6a6b6ee9d5626c8e0a26d
paulparfentii <pavel.parfentii@crisp-studio.com>
2024-09-06 19:39:38 +0300

    added composer.lock1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -116,6 +116,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:send-inactivity-notifications-telegram')->hourly();
 
 //        $schedule->command('app:update-invitees-with-income-telegram')->dailyAt('13:30');
+        //sd
 
     }
 
